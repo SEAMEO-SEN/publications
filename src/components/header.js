0db9b74d263484bddd7f6c1d8476d113209829f9
hurtsky <hurtskyxda@gmail.com>
2019-10-01 16:23:14 +0800

    header: prettier!
    Signed-off-by: hurtsky <hurtskyxda@gmail.com>

--- src/components/header.js
+++ src/components/header.js
@@ -16,10 +16,10 @@ import ExpandMore from "@material-ui/icons/ExpandMore"
 import Collapse from "@material-ui/core/Collapse"
 import ListItemIcon from "@material-ui/core/ListItemIcon"
 import InboxIcon from "@material-ui/icons/MoveToInbox"
-import ListSubheader from '@material-ui/core/ListSubheader';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Divider from '@material-ui/core/Divider';
+import ListSubheader from "@material-ui/core/ListSubheader"
+import ListItemAvatar from "@material-ui/core/ListItemAvatar"
+import Avatar from "@material-ui/core/Avatar"
+import Divider from "@material-ui/core/Divider"
 import { makeStyles, useTheme } from "@material-ui/core/styles"
 import HideOnScrollToDown from "./hideOnScrollToDown"
 import Icse2015 from "../images/icse2015.jpg"
@@ -76,76 +76,74 @@ function Header(props) {
 
   const drawer = (
     <>
-    <List>
-      <ListItemLink href="/">
-        <ListItemText primary="Home" />
-      </ListItemLink>
-    </List>
-    <List
-    subheader={
-      <ListSubheader component="div" id="nested-list-subheader">
-        PROCEEDING
-      </ListSubheader>
-    }
-    >
-      <ListItem button onClick={handleClick}>
-        <ListItemIcon>
-          <InboxIcon />
-        </ListItemIcon>
-        <ListItemText primary={"ICSE"} />
-        {openProc ? <ExpandLess /> : <ExpandMore />}
-      </ListItem>
-      <Collapse in={openProc} timeout="auto" unmountOnExit>
-        <List component="div" disablePadding>
-        <Divider variant="inset" component="li" />
-        <ListItemLink href="/proc2015/" className={classes.nested}>
-            <ListItemAvatar>
-              <Avatar alt="Icon" src={Icse2015} />
-            </ListItemAvatar>
-            <ListItemText 
-            primary="ICSE 2015" 
-            secondary={
-              <React.Fragment>
-                {"ACCESS AND ENGAGEMENT"}
-              </React.Fragment>
-            }
-            />
-          </ListItemLink>
-          <Divider variant="inset" component="li" />
-          <ListItemLink href="/proc2017/" className={classes.nested}>
-            <ListItemAvatar>
-              <Avatar alt="Icon" src={Icse2017} />
-            </ListItemAvatar>
-            <ListItemText 
-            primary="ICSE 2017" 
-            secondary={
-              <React.Fragment>
-                {"ACCESS AND ENGAGEMENT"}
-              </React.Fragment>
-            }
-            />
-          </ListItemLink>
-          <Divider variant="inset" component="li" />
-          <ListItemLink href="/proc2019/" className={classes.nested}>
-            <ListItemAvatar>
-              <Avatar alt="Icon" src={Icse2019} />
-            </ListItemAvatar>
-            <ListItemText 
-            primary="ICSE 2019" 
-            secondary={
-              <React.Fragment>
-                {"ELEVATING INNOVATION FOR SUSTAINABLE DEVELOPMENT OF SPECIAL NEEDS EDUCATION"}
-              </React.Fragment>
-            }
-            />
-          </ListItemLink>
-          <Divider variant="inset" component="li" />
-        </List>
-      </Collapse>
-      <ListItemLink href="/terms/">
-        <ListItemText primary="Terms & Condition" />
-      </ListItemLink>
-    </List>
+      <List>
+        <ListItemLink href="/">
+          <ListItemText primary="Home" />
+        </ListItemLink>
+      </List>
+      <List
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            PROCEEDING
+          </ListSubheader>
+        }
+      >
+        <ListItem button onClick={handleClick}>
+          <ListItemIcon>
+            <InboxIcon />
+          </ListItemIcon>
+          <ListItemText primary={"ICSE"} />
+          {openProc ? <ExpandLess /> : <ExpandMore />}
+        </ListItem>
+        <Collapse in={openProc} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <Divider variant="inset" component="li" />
+            <ListItemLink href="/proc2015/" className={classes.nested}>
+              <ListItemAvatar>
+                <Avatar alt="Icon" src={Icse2015} />
+              </ListItemAvatar>
+              <ListItemText
+                primary="ICSE 2015"
+                secondary={
+                  <React.Fragment>{"ACCESS AND ENGAGEMENT"}</React.Fragment>
+                }
+              />
+            </ListItemLink>
+            <Divider variant="inset" component="li" />
+            <ListItemLink href="/proc2017/" className={classes.nested}>
+              <ListItemAvatar>
+                <Avatar alt="Icon" src={Icse2017} />
+              </ListItemAvatar>
+              <ListItemText
+                primary="ICSE 2017"
+                secondary={
+                  <React.Fragment>{"ACCESS AND ENGAGEMENT"}</React.Fragment>
+                }
+              />
+            </ListItemLink>
+            <Divider variant="inset" component="li" />
+            <ListItemLink href="/proc2019/" className={classes.nested}>
+              <ListItemAvatar>
+                <Avatar alt="Icon" src={Icse2019} />
+              </ListItemAvatar>
+              <ListItemText
+                primary="ICSE 2019"
+                secondary={
+                  <React.Fragment>
+                    {
+                      "ELEVATING INNOVATION FOR SUSTAINABLE DEVELOPMENT OF SPECIAL NEEDS EDUCATION"
+                    }
+                  </React.Fragment>
+                }
+              />
+            </ListItemLink>
+            <Divider variant="inset" component="li" />
+          </List>
+        </Collapse>
+        <ListItemLink href="/terms/">
+          <ListItemText primary="Terms & Condition" />
+        </ListItemLink>
+      </List>
     </>
   )
 
