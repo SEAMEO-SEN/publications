0bfc75cd20c6004381983f629df1333c24898137
hurtsky <hurtskyxda@gmail.com>
2019-10-01 10:35:46 +0800

    layout: add fade animation and use react state
    Signed-off-by: hurtsky <hurtskyxda@gmail.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
 import Header from "./header"
 import Footer from "./footer"
+import Fade from "@material-ui/core/Fade"
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,16 +20,19 @@ const useStyles = makeStyles(theme => ({
 function Layout(props) {
   const { children } = props
   const classes = useStyles()
+  const [state] = useState(false)
 
   return (
     <div className={classes.root}>
       <Header />
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>{children}</Typography>
+        <Fade in={!state}>
+          <Typography paragraph>{children}</Typography>
+        </Fade>
         <div className={classes.toolbar} />
-        <Footer />
       </main>
+      <Footer />
     </div>
   )
 }
