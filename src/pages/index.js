9636ab496b2615e2c719bb675eb2473a65334c61
hurtsky <hurtskyxda@gmail.com>
2019-10-08 16:20:55 +0800

    pages:index: add ICSE information to index page
    Signed-off-by: hurtsky <hurtskyxda@gmail.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,66 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { makeStyles } from "@material-ui/core/styles"
+import Grid from "@material-ui/core/Grid"
+import Card from "@material-ui/core/Card"
+import CardHeader from "@material-ui/core/CardHeader"
+import CardMedia from "@material-ui/core/CardMedia"
+import CardContent from "@material-ui/core/CardContent"
+import CardActions from "@material-ui/core/CardActions"
+import MenuBookIcon from "@material-ui/icons/MenuBook"
+import Button from "@material-ui/core/Button"
+import data from "./index/data"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Home Page</h1>
-  </Layout>
-)
+const useStyles = makeStyles(theme => ({
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "space-between",
+  },
+  media: {
+    height: 500,
+  },
+}))
+
+function IndexPage() {
+  const classes = useStyles()
+
+  return (
+    <Layout>
+      <SEO title="Welcome to SEAMEO SEN Publication Site" />
+      <Grid container spacing={2}>
+        {data.conferences.map((conference, j) => (
+          <Grid key={j} item xs={12} sm={12} md={6} lg={4}>
+            <Card className={classes.card}>
+              <CardMedia
+                className={classes.media}
+                image={conference.confImgCover}
+                title={conference.confName}
+              />
+              <CardHeader
+                title={conference.confName}
+                subheader={conference.confTheme}
+              />
+              <CardContent>{conference.place}</CardContent>
+              <CardActions>
+                <Button
+                  size="big"
+                  color="primary"
+                  href={conference.page}
+                  variant="contained"
+                  startIcon={<MenuBookIcon />}
+                >
+                  READ MORE
+                </Button>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Layout>
+  )
+}
 
 export default IndexPage
