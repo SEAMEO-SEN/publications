d35ade8e5548b3b71e2332f8df58f0bdeb2d7278
hurtsky <hurtskyxda@gmail.com>
2019-10-09 15:15:48 +0800

    package: add gatsby-plugin-gtag plugin
    Signed-off-by: hurtsky <hurtskyxda@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,17 @@ module.exports = {
         }
       }
     },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingId: `UA-149707309-1`,
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     `gatsby-plugin-offline`,
