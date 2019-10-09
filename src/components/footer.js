cfc65474370d2251aee8b7b10e5a832d75443cb4
hurtsky <hurtskyxda@gmail.com>
2019-10-09 12:28:21 +0800

    footer: add social button and terms link
    Signed-off-by: hurtsky <hurtskyxda@gmail.com>

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,24 +1,47 @@
 import React from "react"
 import { makeStyles } from "@material-ui/core/styles"
+import FacebookIcon from "@material-ui/icons/Facebook"
+import TwitterIcon from "@material-ui/icons/Twitter"
+import YouTubeIcon from "@material-ui/icons/YouTube"
+import Button from "@material-ui/core/Button"
 
 const drawerWidth = 260
 
 const useStyles = makeStyles(theme => ({
   footer: {
-    background: "#3f51b5",
+    display: "flex",
+    flexDirection: "row",
+    paddingTop: 10,
+    paddingBottom: 10,
+    background: "#283593",
     color: "white",
-    paddingLeft: 0,
+    justifyContent: "center",
     [theme.breakpoints.up("sm")]: {
       paddingLeft: drawerWidth,
     },
+  },
+  whiteColor: {
+    color: "white",
   },
 }))
 
 function Footer() {
   const classes = useStyles()
+
   return (
     <div className={classes.footer}>
-      Copyright &copy; 2019 SEAMEO SEN All Rights Reserved
+      <Button size="big" href="https://facebook.seameosen.org" target="_blank" rel="noopener">
+        <FacebookIcon className={classes.whiteColor} />
+      </Button>
+      <Button size="big" href="https://twitter.seameosen.org" target="_blank" rel="noopener">
+        <TwitterIcon className={classes.whiteColor} />
+      </Button>
+      <Button size="big" href="https://youtube.seameosen.org" target="_blank" rel="noopener">
+        <YouTubeIcon className={classes.whiteColor} />
+      </Button>
+      <Button size="big" href="/terms/" className={classes.whiteColor}>
+        Copyright &copy; 2019 SEAMEO SEN All Rights Reserved
+      </Button>
     </div>
   )
 }
