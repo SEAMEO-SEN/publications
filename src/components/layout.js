cc990412c7b8187e04b4c4bbab05326205866989
hurtsky <hurtskyxda@gmail.com>
2021-03-14 15:42:09 +0800

    components:layout: remove root class
    * appbar now no need this
    
    Signed-off-by: hurtsky <hurtskyxda@gmail.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,20 +11,13 @@ import Fab from "@material-ui/core/Fab"
 import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp"
 import ScrollTop from "./scrollToTop"
 
-const drawerWidth = 260
-
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    minHeight: "100vh",
-    flexDirection: "column",
-  },
   toolbar: theme.mixins.toolbar,
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
     [theme.breakpoints.up("sm")]: {
-      paddingLeft: drawerWidth,
+      paddingLeft: 260,
     },
   },
 }))
@@ -50,20 +43,18 @@ const Layout = ({ children, props }) => {
 
   return (
     <ThemeProvider theme={muiTheme}>
-      <div className={classes.root}>
-        <Header onToggleDark={toggleDarkTheme} />
-        <main className={classes.content}>
-          <div className={classes.toolbar} id="back-to-top-anchor" />
-          <div>{children}</div>
-          <div className={classes.toolbar} />
-          <ScrollTop {...props}>
-            <Fab color="secondary" size="small" aria-label="scroll back to top">
-              <KeyboardArrowUpIcon />
-            </Fab>
-          </ScrollTop>
-        </main>
-        <Footer />
-      </div>
+      <Header onToggleDark={toggleDarkTheme} />
+      <main className={classes.content}>
+        <div className={classes.toolbar} id="back-to-top-anchor" />
+        <div>{children}</div>
+        <div className={classes.toolbar} />
+        <ScrollTop {...props}>
+          <Fab color="secondary" size="small" aria-label="scroll back to top">
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </ScrollTop>
+      </main>
+      <Footer />
     </ThemeProvider>
   )
 }
