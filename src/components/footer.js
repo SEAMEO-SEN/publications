e59c47d970d2b428eea4745593c43a3e5566dcce
hurtsky <hurtskyxda@gmail.com>
2021-03-14 15:41:18 +0800

    components:footer: use AppBar and inherit the color from theme
    Signed-off-by: hurtsky <hurtskyxda@gmail.com>

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,24 +4,20 @@ import FacebookIcon from "@material-ui/icons/Facebook"
 import TwitterIcon from "@material-ui/icons/Twitter"
 import YouTubeIcon from "@material-ui/icons/YouTube"
 import Button from "@material-ui/core/Button"
+import AppBar from "@material-ui/core/AppBar"
+import Toolbar from "@material-ui/core/Toolbar"
 
-const drawerWidth = 260
-
-const useStyles = makeStyles(theme => ({
-  footer: {
-    display: "flex",
-    flexDirection: "row",
-    paddingTop: 10,
-    paddingBottom: 10,
-    background: "#283593",
-    color: "white",
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    top: "auto",
+    bottom: 0,
     justifyContent: "center",
     [theme.breakpoints.up("sm")]: {
-      paddingLeft: drawerWidth,
+      paddingLeft: 260,
     },
   },
-  whiteColor: {
-    color: "white",
+  title: {
+    flexGrow: 1,
   },
 }))
 
@@ -29,35 +25,42 @@ const Footer = () => {
   const classes = useStyles()
 
   return (
-    <div className={classes.footer}>
-      <Button
-        size="large"
-        href="https://facebook.seameosen.org"
-        target="_blank"
-        rel="noopener"
-      >
-        <FacebookIcon className={classes.whiteColor} />
-      </Button>
-      <Button
-        size="large"
-        href="https://twitter.seameosen.org"
-        target="_blank"
-        rel="noopener"
-      >
-        <TwitterIcon className={classes.whiteColor} />
-      </Button>
-      <Button
-        size="large"
-        href="https://youtube.seameosen.org"
-        target="_blank"
-        rel="noopener"
-      >
-        <YouTubeIcon className={classes.whiteColor} />
-      </Button>
-      <Button size="large" href="/terms/" className={classes.whiteColor}>
-        Copyright &copy; {(new Date().getFullYear())} SEAMEO SEN All Rights Reserved
-      </Button>
-    </div>
+    <AppBar className={classes.appBar}>
+      <Toolbar>
+        <Button
+          size="large"
+          href="https://facebook.seameosen.org"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+        >
+          <FacebookIcon />
+        </Button>
+        <Button
+          size="large"
+          href="https://twitter.seameosen.org"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+        >
+          <TwitterIcon />
+        </Button>
+        <Button
+          size="large"
+          href="https://youtube.seameosen.org"
+          target="_blank"
+          rel="noopener"
+          color="inherit"
+        >
+          <YouTubeIcon />
+        </Button>
+        <div className={classes.title} />
+        <Button size="large" href="/terms/" color="inherit">
+          Copyright &copy; {new Date().getFullYear()} SEAMEO SEN All Rights
+          Reserved
+        </Button>
+      </Toolbar>
+    </AppBar>
   )
 }
 
